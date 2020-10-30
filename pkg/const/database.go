a98c0a28bd951887b207e973e138f838b3348baf
Bilal <kocogluubilal@gmail.com>
2020-10-30 22:14:46 +0300

    db connection for environment

---
+++ pkg/const/database.go
@@ -0,0 +1,9 @@
+package _const
+
+const (
+	DefaultDbHost     = "localhost"
+	DefaultDbPort     = 3306
+	DefaultDbUser     = "root"
+	DefaultDbPassword = "admin"
+	DefaultDbName     = "file-service"
+)
